22175cf8d34c72d4fb7964732bc2a02d796b477a
Kiwon Hong <rusdlsla@gmail.com>
2019-07-21 15:21:14 +0900

    home component 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Home</h2>
+    <div>{{ greeting }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from 'axios'
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  date() {
+    return {
+      greeting: ''
+    }
+  },
+  created() {
+    axios.get('http://localhost:8088/home')
+      .then(result => this.greeting = result.data.greeting)
   }
 };
 </script>
