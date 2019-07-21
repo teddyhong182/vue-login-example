10d4ffceca4d49fc389f904422a1895e17dac790
Kiwon Hong <rusdlsla@gmail.com>
2019-07-21 15:24:13 +0900

    정의한 component 추가

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
+import MyPage from "./views/MyPage.vue";
 
 Vue.use(Router);
 
---
+++ src/views/MyPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mypage">
+    <h1>This is an MyPage page</h1>
+  </div>
+</template>
